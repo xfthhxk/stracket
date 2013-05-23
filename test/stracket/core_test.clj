17620134e3299815d0a9aa9f361cc2e78d5bfe7d
xfthhxk <xfthhxk@gmail.com>
2013-05-23 19:38:27 -0400

    reorder facts

--- test/stracket/core_test.clj
+++ test/stracket/core_test.clj
@@ -3,6 +3,15 @@
   (require [stracket.core :as s]
            [stracket.search :as ss]
            [stracket.constraint :as sc]))
+
+(fact "test fd-vars macro"
+  (let [j-store (s/store)
+        shoes-map (s/fd-vars {:store j-store :min 1 :max 4}
+                             :EcruEspadrilles :FuchsiaFlats :PurplePumps :SuedeSandals)]
+    shoes-map => map?
+    (:SuedeSandals shoes-map) => s/int-var?
+    (-> shoes-map :SuedeSandals .min) => 1
+    (-> shoes-map :SuedeSandals .max) => 4))
   
 (fact "JaCoP map coloring example"
   (let [store (s/store)
@@ -16,59 +25,53 @@
     (s/extract-var-info store) => {:V4 1, :V3 2, :V2 1, :V1 3}))
 
 
-(fact "defvars test"
-  (def jacop-store (s/store))
-      
-  (s/defvars shoes
-    {:store jacop-store :min 1 :max 4}
-    :EcruEspadrilles :FuchsiaFlats :PurplePumps :SuedeSandals)
-  
-  (s/defvars stores
-    {:store jacop-store :min 1 :max 4}
-    :FootFarm :HeelsInAHandcart :TheShoePlace :Tootsies)
+(facts "ArchFriends test"
+       (fact "defvars test"
+             (def jacop-store (s/store))
+             
+             (s/defvars shoes
+               {:store jacop-store :min 1 :max 4}
+               :EcruEspadrilles :FuchsiaFlats :PurplePumps :SuedeSandals)
+             
+             (s/defvars stores
+               {:store jacop-store :min 1 :max 4}
+               :FootFarm :HeelsInAHandcart :TheShoePlace :Tootsies)
 
-  jacop-store => s/store?
-  
-  shoes => map?
-  (:FuchsiaFlats shoes) => s/int-var?
-  (-> shoes :FuchsiaFlats .min) => 1
-  (-> shoes :FuchsiaFlats .max) => 4
+             jacop-store => s/store?
+             
+             shoes => map?
+             (:FuchsiaFlats shoes) => s/int-var?
+             (-> shoes :FuchsiaFlats .min) => 1
+             (-> shoes :FuchsiaFlats .max) => 4
+             
+             stores => map?
+             (:FootFarm stores) => s/int-var?
+             (-> stores :FootFarm .min) => 1
+             (-> stores :FootFarm .max) => 4)
+       
+       
+       (fact "defconstraints test"
+             (s/defconstraints constraints
+               (sc/all-different (vals shoes))
+               (sc/all-different (vals stores))
+               (sc/eq (:FuchsiaFlats shoes) (:HeelsInAHandcart stores))
+               (sc/notc (sc/x+c=z (:PurplePumps shoes) 1 (:Tootsies stores)))
+               (sc/eq (:FootFarm stores) 2)
+               (sc/x+c=z (:TheShoePlace stores) 2 (:SuedeSandals shoes)))
+             
+             constraints => vector?)
+       
+       (fact "test Arch Friends result"
+             (s/impose! jacop-store constraints)
+             (ss/search-all-at-once jacop-store) ;; this mutates the vars created with defvars      
+             (s/extract-var-info jacop-store) => {:EcruEspadrilles 2
+                                                  :FuchsiaFlats 4
+                                                  :PurplePumps 1
+                                                  :SuedeSandals 3
+                                                  :FootFarm 2
+                                                  :HeelsInAHandcart 4
+                                                  :TheShoePlace 1
+                                                  :Tootsies 3}))
 
-  stores => map?
-  (:FootFarm stores) => s/int-var?
-  (-> stores :FootFarm .min) => 1
-  (-> stores :FootFarm .max) => 4)
 
-
-(fact "defconstraints test"
-  (s/defconstraints constraints
-    (sc/all-different (vals shoes))
-    (sc/all-different (vals stores))
-    (sc/eq (:FuchsiaFlats shoes) (:HeelsInAHandcart stores))
-    (sc/notc (sc/x+c=z (:PurplePumps shoes) 1 (:Tootsies stores)))
-    (sc/eq (:FootFarm stores) 2)
-    (sc/x+c=z (:TheShoePlace stores) 2 (:SuedeSandals shoes)))
-
-  constraints => vector?)
-
-(fact "test Arch Friends result"
-  (s/impose! jacop-store constraints)
-  (ss/search-all-at-once jacop-store) ;; this mutates the vars created with defvars      
-  (s/extract-var-info jacop-store) => {:EcruEspadrilles 2
-                                       :FuchsiaFlats 4
-                                       :PurplePumps 1
-                                       :SuedeSandals 3
-                                       :FootFarm 2
-                                       :HeelsInAHandcart 4
-                                       :TheShoePlace 1
-                                       :Tootsies 3})
-
-(fact "test fd-vars macro"
-  (let [j-store (s/store)
-        shoes-map (s/fd-vars {:store jacop-store :min 1 :max 4}
-                             :EcruEspadrilles :FuchsiaFlats :PurplePumps :SuedeSandals)]
-    shoes-map => map?
-    (:SuedeSandals shoes-map) => s/int-var?
-    (-> shoes-map :SuedeSandals .min) => 1
-    (-> shoes-map :SuedeSandals .max) => 4))
 
